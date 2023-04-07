8295dfbfee30da57bca9aff5903e7c965141536b
YukisawaNya <rainbowbedrock@outlook.com>
2023-04-07 12:58:41 +0800

    fix something :P

--- scripts/lazyload.js
+++ scripts/lazyload.js
@@ -4,7 +4,7 @@ hexo.extend.filter.register(
   'after_post_render',
   function (data) {
     var theme = hexo.theme.config
-    if (theme.lazyload.open!==true) return;
+    if (theme.lazyloadenabled!==true) return;
 
     data.content = data.content.replace(
       // Match 'img' tags width the src attribute.
